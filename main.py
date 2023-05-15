eab259934e5d1ef45a6dd647b01d2b9c7c0c537e
ofao <131997134+ofao@users.noreply.github.com>
2023-05-15 21:05:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from kivy.uix.textinput import TextInput
 #from kivymd.uix.button import MDFlatButton
 #from kivymd.uix.textfield import MDTextField
 from kivymd.uix.card import MDCard
-import sqlite3
+import sqlite3, pickle
 from board import *  #введение
-from kivy.uix.boxlayout import BoxLayout
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.uix.image import Image
 
 class MyApp(MDApp):
@@ -22,8 +22,7 @@ class MyApp(MDApp):
         self.label = MDLabel(text = 'Рабочая тетрадь SQLite')
         #self.input = TextInput(hint_text = 'SQLite commands...', multiline = True)
         #b2e6f0
-        self.board = OnBoarding()
-        
+        self.box = MDBoxLayout(adaptive_size = True, spacing = 15, pos_hint = {'top': 1})
     def on_text(self, *args):
         try:
             connection = sqlite3.connect('sq.db')
@@ -34,21 +33,36 @@ class MyApp(MDApp):
         except Exception as e: print(e)
       
     def build(self):
-        self.screen.add_widget(self.board)
+        with open('data.pickle', 'rb') as f:
+            obuch_passed = pickle.load(f)
+        if obuch_passed == False:
+            self.screen.add_widget(Onboarding())
+        else:
+            card1 = MD3Card(text = 'Реляционная алгебра')
+            card2 = MD3Card(text = 'Тимка любимая попка')
+            card3 = MD3Card(text = 'Я тебя люблюююю\nмилый')
+            self.box.add_widget(card1)
+            self.box.add_widget(card2)
+            self.box.add_widget(card3)
+            self.screen.add_widget(self.box)
         return self.screen
     
-class OnBoarding(MDScreen):
+class Onboarding(MDScreen):
     def finish_callback(self):
-        myapp.screen.remove_widget(myapp.board)
-        myapp.screen.add_widget(MD3Card())
+        myapp.screen.remove_widget(self)
+        with open('data.pickle', 'wb') as f:
+            obuch_passed = True
+        self.screen.add_widget(BoxLayout().add_widget(MD3Card()))
         
     def change_theme(self):
         myapp.theme_cls.theme_style = (
                 'Dark' if myapp.theme_cls.theme_style == 'Light' else 'Light')
-        print(myapp.theme_cls.text_color)
 
 class MD3Card(MDCard):
-    pass
+    text = StringProperty()
+
+    def change_widget(self):
+        pass
 if __name__ == '__main__':
     myapp = MyApp()
     myapp.run()
